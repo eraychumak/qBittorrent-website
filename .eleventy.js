c29c8b207c5a7e9b90263768e1eeef949af38809
Eray Chumak <eray_chumak@protonmail.com>
2023-08-27 03:33:00 +0100

    Don't error on missing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(require("eleventy-plugin-heroicons"), {
-    className: "heroIcon"
+    className: "heroIcon",
+    errorOnMissing: false
   });
 
   eleventyConfig.addPassthroughCopy("src/css")
